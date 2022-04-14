afa6e125922d4ec5f340393eea7b06b60631c8e8
Onur Omer SAHIN <onuromersahin@gmail.com>
2022-04-14 21:35:11 +0300

    making so much good progress

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route()
+     */
+    public function homepage()
+    {
+        return new Response("This is my first page!");
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf("Future page to show the article: %s",$slug));
+    }
+}
