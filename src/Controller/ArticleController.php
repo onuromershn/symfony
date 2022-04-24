e584345d683d8596861cb2fea22cd05838ac41e7
Onur Omer SAHIN <onuromersahin@gmail.com>
2022-04-24 14:46:33 +0300

    creating service

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,11 +2,9 @@
 
 namespace App\Controller;
 
-use Michelf\Markdown;
-use Michelf\MarkdownInterface;
+use App\Service\MarkdownHelper;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,7 +21,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}",name="show")
      */
-    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
         $comments = ["Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "];
@@ -32,13 +30,7 @@ class ArticleController extends AbstractController
 It is a **long established** fact that a reader will be distracted by the readable [beef ribs](https://baconipsum.com/) of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using **'Content here, content here'**, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 EOF;
 
-        $item = $cache->getItem('markdown_'.md5($articleContent));
-        if (!$item->isHit()){
-            $item->set($markdown->transform($articleContent));
-            $cache->save($item);
-        }
-
-        $articleContent = $item->get();
+        $articleContent = $markdownHelper->parse($articleContent);
 
         return $this->render('article/show.html.twig',[
             'title'=>ucwords(str_replace('-',' ',$slug)),
